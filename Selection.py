f41d96d371749b734feddbe3687f07cd19724e7e
Prakhar Saxena <91530129+Someone-who-loves-coding@users.noreply.github.com>
2023-10-25 20:47:47 +0530

    Create Selection.py

---
+++ Selection.py
@@ -0,0 +1,19 @@
+def Selection_Sort(arr, n):
+	
+	for i in range(n - 1):
+		min_index = i 
+		
+		for j in range(i + 1, n):
+			if (arr[j] < arr[min_index]):
+				min_index = j
+				
+		arr[i], arr[min_index] = arr[min_index], arr[i] 
+		
+n = 5
+arr = [ 2, 0, 1, 4, 3 ]
+Selection_Sort(arr, n)
+
+print("The Sorted Array by using " \
+	"Selection Sort is : ", end = '')
+for i in range(n):
+	print(arr[i], end = " ")
